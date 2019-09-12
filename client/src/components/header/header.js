969782f31b4947759fe5fd1f1d077c7dfc3b2c30
maasai <jkemboe@gmail.com>
2019-09-12 13:52:02 +0300

    Frontend UI with Max stack

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router,Link} from 'react-router-dom';
 import '../header/header.css';
-
+import PersonSvg from './img/icon.svg';
 function Header(){
     return (
         <div className="top-nav">
@@ -9,8 +9,10 @@ function Header(){
                 <h1>BCC</h1>
              </div>
             <Router>
+                <div className="icon-nav-link">
+                    <Link to="#" className="sign"><img src={PersonSvg} alt="icon"/></Link>
+                </div>
                 <div className="top-nav-link">
-                    <Link to="#" className="sign">Sign In</Link>
                     <Link to="#">World</Link>
                     <Link to="#">US Politics</Link>
                     <Link to="#">Business</Link>
@@ -22,7 +24,7 @@ function Header(){
                 </div>
             </Router>
             <div className="top-nav-search">
-                <input type="text" placeholder="search"></input>
+                <input type="text" placeholder="Search"></input>
             </div>
         </div>
     )
--- client/src/components/section/section.js
+++ client/src/components/section/section.js
@@ -4,11 +4,26 @@ import '../section/section.css';
 
 function Section(){
     return(
+
         <div className="section-body">
             <Router>
-                <div className="section-link">
-                    <Link to="#">Business</Link>
+            <div className="section-link">
+        <Link to="#">Business</Link>
+    </div>
+    <div className="main-section">
+    <div className="section-head">
+                <h2>Blog Title</h2>
+                <img alt="Section Image"/>
+                <span className="author-par">Author: <Link to="#" className="author">Maasai Sang</Link></span>
+                <div className="section-content">
+                    <p>Lorem Ipsum: Author: Maasai Sang
+                    Lorem Ipsum: Author: 
+                    </p>
                 </div>
+             
+                <Link to="#" className="read-more">Read More</Link>
+                <span className="date-style">Date: 09/9/2019</span>
+            </div>
             <div className="section-head">
                 <h2>Blog Title</h2>
                 <img alt="Section Image"/>
@@ -22,6 +37,85 @@ function Section(){
                 <Link to="#" className="read-more">Read More</Link>
                 <span className="date-style">Date: 09/9/2019</span>
             </div>
+            <div className="section-head">
+                <h2>Blog Title</h2>
+                <img alt="Section Image"/>
+                <span className="author-par">Author: <Link to="#" className="author">Maasai Sang</Link></span>
+                <div className="section-content">
+                    <p>Lorem Ipsum: Author: Maasai Sang
+                    Lorem Ipsum: Author: 
+                    </p>
+                </div>
+             
+                <Link to="#" className="read-more">Read More</Link>
+                <span className="date-style">Date: 09/9/2019</span>
+            </div>
+            <div className="section-head">
+                <h2>Blog Title</h2>
+                <img alt="Section Image"/>
+                <span className="author-par">Author: <Link to="#" className="author">Maasai Sang</Link></span>
+                <div className="section-content">
+                    <p>Lorem Ipsum: Author: Maasai Sang
+                    Lorem Ipsum: Author: 
+                    </p>
+                </div>
+             
+                <Link to="#" className="read-more">Read More</Link>
+                <span className="date-style">Date: 09/9/2019</span>
+            </div>
+            <div className="section-head">
+                <h2>Blog Title</h2>
+                <img alt="Section Image"/>
+                <span className="author-par">Author: <Link to="#" className="author">Maasai Sang</Link></span>
+                <div className="section-content">
+                    <p>Lorem Ipsum: Author: Maasai Sang
+                    Lorem Ipsum: Author: 
+                    </p>
+                </div>
+             
+                <Link to="#" className="read-more">Read More</Link>
+                <span className="date-style">Date: 09/9/2019</span>
+            </div>
+            <div className="section-head">
+                <h2>Blog Title</h2>
+                <img alt="Section Image"/>
+                <span className="author-par">Author: <Link to="#" className="author">Maasai Sang</Link></span>
+                <div className="section-content">
+                    <p>Lorem Ipsum: Author: Maasai Sang
+                    Lorem Ipsum: Author: 
+                    </p>
+                </div>
+             
+                <Link to="#" className="read-more">Read More</Link>
+                <span className="date-style">Date: 09/9/2019</span>
+            </div>
+            <div className="section-head">
+                <h2>Blog Title</h2>
+                <img alt="Section Image"/>
+                <span className="author-par">Author: <Link to="#" className="author">Maasai Sang</Link></span>
+                <div className="section-content">
+                    <p>Lorem Ipsum: Author: Maasai Sang
+                    Lorem Ipsum: Author: 
+                    </p>
+                </div>
+             
+                <Link to="#" className="read-more">Read More</Link>
+                <span className="date-style">Date: 09/9/2019</span>
+            </div>
+            <div className="section-head">
+                <h2>Blog Title</h2>
+                <img alt="Section Image"/>
+                <span className="author-par">Author: <Link to="#" className="author">Maasai Sang</Link></span>
+                <div className="section-content">
+                    <p>Lorem Ipsum: Author: Maasai Sang
+                    Lorem Ipsum: Author: 
+                    </p>
+                </div>
+             
+                <Link to="#" className="read-more">Read More</Link>
+                <span className="date-style">Date: 09/9/2019</span>
+            </div>
+    </div>
             </Router>
            
         </div>
