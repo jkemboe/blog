284de92a9636f20b93791dc32dd4c4c1d95e1867
maasai <jkemboe@gmail.com>
2019-09-13 16:16:04 +0300

    Added admin route

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
 import Header from './components/header/header';
 import Section from './components/section/section';
+
+import AdminPanel from './components/adminpanel/adminpanel';
 import './App.css';
+
+function Main() {
+  return (
+    <Router>
+        <Route exact path="/" component={App}/>
+        <Route path="/admin" component={AdminPanel}/>
+    </Router>
+  )
+}
 
 function App() {
   return (
@@ -14,4 +27,6 @@ function App() {
   );
 }
 
-export default App;
+
+
+export default Main;
---
+++ client/src/components/adminpanel/adminpanel.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './adminpanel.css';
+
+class AdminPanel extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>ADMIN PANEL</h1>
+            </div>
+        )
+    }
+
+}
+ 
+ 
+
+export default AdminPanel;
